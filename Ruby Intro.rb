60a3ff2fe914d8926a615f327122911242a2859a
yihongme <yihong703@gmail.com>
2021-07-16 22:21:37 +0800

    First project

--- Ruby Intro.rb
+++ Ruby Intro.rb
@@ -2,33 +2,28 @@
 
 # Part 1
 
-def sum arr
+def sum(arr)
   # YOUR CODE HERE
   arr.inject(0, :+)
 end
 
-def max_2_sum arr
+def max_2_sum(arr)
   # YOUR CODE HERE
   sum(arr.max(2))
 end
 
-def sum_to_n? arr, n
+def sum_to_n?(arr, n)
   # YOUR CODE HERE
-  if arr.length < 2
-    return false
-  end
+  return false if arr.length < 2
 
-  for element in arr do
-    residual = n- element
-    if arr.include?(residual)
+  arr.each do |element|
+    residual = n - element
+    next unless arr.include?(residual)
 
-      # in case exactly one item is half of the target, the residual method may not work
-      if residual == element and arr.count(element)==1
-        return false
-      end
+    # in case exactly one item is half of the target, the residual method may not work
+    return false if (residual == element) && (arr.count(element) == 1)
 
-      return true
-    end
+    return true
   end
   false
 end
@@ -42,24 +37,19 @@ end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
-  if (s.length == 0) or (not s[0] =~ /[A-Za-z]/)
-    return false
-  end
+  return false if s.length.zero? || (s[0] !~ /[A-Za-z]/)
 
-  not ["A", "E", "I", "O", "U"].include?(s[0].capitalize)
+  !%w[A E I O U].include?(s[0].capitalize)
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
-  if not s !~ /[^01]/ or s.length == 0
-    return false
-  end
+  return false if s =~ (/[^01]/) || s.length.zero?
 
-  converted_int = s.to_i(base=2)
+  converted_int = s.to_i(base = 2)
 
-  if converted_int % 4 ==0
-    return true
-  end
+  return true if (converted_int % 4).zero?
+
   false
 end
 
@@ -75,25 +65,17 @@ class BookInStock
   end
 
   # getter
-  def isbn
-    @isbn
-  end
+  attr_reader :isbn
 
-  def price
-    @price
-  end
+  attr_reader :price
 
   # setter
-  def isbn=(isbn)
-    @isbn = isbn
-  end
+  attr_writer :isbn
 
-  def price=(price)
-    @price = price
-  end
+  attr_writer :price
 
   def price_as_string
-    "$" + '%.2f' % (@price)
+    "$#{format('%.2f', @price)}"
   end
 
 end
